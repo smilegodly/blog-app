74fd2968e25a9ced469bfd7414e21e1542571b42
Matthew Clifford <35276477+smilegodly@users.noreply.github.com>
2021-04-12 13:28:21 -0400

    Refactored Action Creator to now only fetch unique user ids from jsonplaceholder api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,16 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
